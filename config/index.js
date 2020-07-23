e4f2f892222ed366e7e5e38df06d991f6a4fe226
fernando <fernan942@gmail.com>
2020-07-23 09:42:38 -0400

    Connection to MongoAtlas: An instance of MongoDB 25

--- config/index.js
+++ config/index.js
@@ -3,6 +3,11 @@ require('dotenv').config();
 const config = {
     dev: process.env.NODE_ENV !== 'production',
     port: process.env.PORT || 3000,
+    cors: process.env.CORS,
+    dbUser: process.env.dbUser,
+    dbPassword: process.env.dbPassword,
+    dbHost: process.env.dbHost,
+    dbName: process.env.dbName,
 };
 
 module.exports = { config };
