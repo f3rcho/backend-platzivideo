7c6616c11e3f8912da96b618a15f0427406ebe76
fernando <fernan942@gmail.com>
2020-07-26 19:16:55 -0400

    MongoDB services connection 28

--- config/index.js
+++ config/index.js
@@ -1,13 +1,11 @@
-require('dotenv').config();
-
-const config = {
-    dev: process.env.NODE_ENV !== 'production',
-    port: process.env.PORT || 3000,
-    cors: process.env.CORS,
-    dbUser: process.env.dbUser,
-    dbPassword: process.env.dbPassword,
-    dbHost: process.env.dbHost,
-    dbName: process.env.dbName,
+module.exports = {
+    api: {
+        dev: process.env.NODE_ENV || 'production',
+        port: process.env.PORT || 3000,
+        cors: process.env.CORS,
+        dbUser: process.env.dbUser || 'db_platziVideos',
+        dbPassword: process.env.dbPassword || 'hMyIfM631oQMHIT4',
+        dbHost: process.env.dbHost || 'cluster0-icc7n.mongodb.net',
+        dbName: process.env.dbName || 'platziVideos',
+    }
 };
-
-module.exports = { config };
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 
-const { config } = require('./config/index');
+const config = require('./config/index');
 const moviesApi = require('./routes/movies');
+
+//body parser
+app.use(express.json())
 
 moviesApi(app);
 
 
-app.listen(config.port, function() {
+app.listen(config.api.port, function() {
     // eslint-disable-next-line no-console
-    console.log(`App listening on http://localhost:${config.port}`);
+    console.log(`App listening on http://localhost:${config.api.port}`);
 });
